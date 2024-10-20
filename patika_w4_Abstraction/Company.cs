9179289c9fe90ad60e934dc16861e5ffa76b95ef
YasarDogann <yasardgn99@gmail.com>
2024-10-20 12:54:23 +0300

    abstraction

---
+++ patika_w4_Abstraction/Company.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patika_w4_Abstraction
+{
+    //IDepartment arayüzü: tüm departmanlar için bir görev methodu tanımlar
+    public interface IDepartment
+    {
+        void PerformTask();  // Görev Methodu tanımlanması
+    }
+
+    // Company sınıfı : Tüm çalışanlar için ortak özellikleri barındıran abstract bir sınıf
+    public abstract class Company : IDepartment
+    {
+        // Çalışan için ad soyad ve pozisyon için ortak özellikler (Properties)
+        public string Name { get; set; }
+        public string LastName {  get; set; }
+        public string Position { get; set; }
+
+
+        // Company sınıfı yapıcı methodu.
+        public Company(string name, string lastname,string position)
+        {
+            Name = name;
+            LastName = lastname;
+            Position = position;
+        }
+
+        // soyut görev methodu: türeyen sınıflar tarafından kullanılmak zorundadır.
+        public abstract void PerformTask();
+    }
+
+
+    // Employee sınıfı: Company sınıfından miras almıştır. Çalışan için özel görev davranışını tanımlar
+    public class Employee : Company
+    {
+        // Emplooye yapıcı methodu: Company sınıfı yapıcı methoduna parametreleri iletir.
+        public Employee(string name, string lastname,string position) : base(name, lastname, position) { }
+
+        // PerformTask methodu override eder. Çalışanın isim soyisim ve görevini ekrana yazdırır.
+        public override void PerformTask()
+        {
+            Console.WriteLine($"{Name} {LastName} şirketin {Position} olarak çalışıyor");
+        }
+    }
+}
---
+++ patika_w4_Abstraction/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace patika_w4_Abstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Yaşar", "Doğan", "Yazılım Mühendisi");
+            employee.PerformTask();
+
+            Console.WriteLine("\r\n*********************************************************\r\n");
+
+            Employee employee2 = new Employee("Hasan", "Çıldırmış", "Proje Yöneticisi");
+            employee2.PerformTask();
+        }
+    }
+}
